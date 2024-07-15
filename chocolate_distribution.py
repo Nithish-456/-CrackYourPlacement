cda75d63bfc85961401f627b6c76a99e51cac90a
Nithish Paidimarri <126104830+Nithish-456@users.noreply.github.com>
2024-07-15 22:55:28 +0530

    Day2

---
+++ chocolate_distribution.py
@@ -0,0 +1,13 @@
+def findMinDiff(self, A,N,M):
+
+        if (M == 0 or N == 0):
+            return 0
+        
+        A.sort()
+        if (N<M):
+            return -1
+        min_diff = A[N-1] - A[0]
+        for i in range(len(A) - M + 1):
+            min_diff = min(min_diff,  A[i + M - 1] - A[i])
+ 
+        return min_diff
