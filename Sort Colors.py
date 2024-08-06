483e3cc4546b100801cf83083fbebdd03b180bfe
Nithish Paidimarri <126104830+Nithish-456@users.noreply.github.com>
2024-08-06 23:01:31 +0530

    Day 24

---
+++ Sort Colors.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        low = 0
+        high = n - 1
+        i = 0
+
+        while i <= high:
+            
+            if nums[i] == 0:
+                nums[i], nums[low] = nums[low], nums[i]
+                low += 1
+            
+            elif nums[i] == 2:
+                nums[i], nums[high] = nums[high], nums[i]
+                high -= 1
+                i -= 1
+            i += 1
+        
