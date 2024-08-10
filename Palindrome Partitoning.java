d17f47c3c8f4297be774705f1a49b84ebb6b7775
Nithish Paidimarri <126104830+Nithish-456@users.noreply.github.com>
2024-08-10 17:09:05 +0530

    Day 27

---
+++ Palindrome Partitoning.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), res);
+        return res;
+    }
+    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+        // If we've reached the end of the string, add the current partition to the result list
+        if (start == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        // Explore all possible partitions
+        for (int end = start + 1; end <= s.length(); end++) {
+            // If the current substring is a palindrome, add it to the current path
+            if (ispalin(s, start, end - 1)) {
+                path.add(s.substring(start, end));
+                // Recur to find other partitions
+                backtrack(s, end, path, result);
+                // Backtrack to explore other partitions
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+    private boolean ispalin(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
