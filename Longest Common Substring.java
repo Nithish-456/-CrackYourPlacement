bc94d3b9855e130ee6f49d748af0ed7d2fa5410d
Nithish Paidimarri <126104830+Nithish-456@users.noreply.github.com>
2024-08-19 14:54:39 +0530

    d37

---
+++ Longest Common Substring.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestCommonSubstr(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        
+        int[][] dp = new int[n+1][m+1];
+        int max_len = 0;
+        for (int i=1; i<=n;i++){
+            for (int j=1;j<=m;j++){
+                if (str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    max_len = Math.max(max_len, dp[i][j]);
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return max_len;
+    }
+}
