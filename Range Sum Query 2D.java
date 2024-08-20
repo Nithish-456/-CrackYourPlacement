f7c47e778061407caad5ff8eabba81a0003fc208
Nithish Paidimarri <126104830+Nithish-456@users.noreply.github.com>
2024-08-20 19:19:53 +0530

    d38

---
+++ Range Sum Query 2D.java
@@ -0,0 +1,26 @@
+class NumMatrix {
+    private int[][] prefixSum;
+
+    public NumMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        prefixSum = new int[rows + 1][cols + 1];
+
+        // Compute prefix sum
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                prefixSum[i][j] = matrix[i - 1][j - 1] 
+                                + prefixSum[i - 1][j] 
+                                + prefixSum[i][j - 1] 
+                                - prefixSum[i - 1][j - 1];
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefixSum[row2 + 1][col2 + 1] 
+             - prefixSum[row1][col2 + 1] 
+             - prefixSum[row2 + 1][col1] 
+             + prefixSum[row1][col1];
+    }
+}
