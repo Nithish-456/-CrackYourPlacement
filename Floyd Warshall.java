1a36ff754a39a8095ad75ec6b2627358e026ed0b
Nithish Paidimarri <126104830+Nithish-456@users.noreply.github.com>
2024-08-25 12:31:08 +0530

    d43

---
+++ Floyd Warshall.java
@@ -0,0 +1,34 @@
+
+
+//User function template for JAVA
+
+class Solution {
+    public void shortest_distance(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1 && i != j) {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        // Floyd-Warshall algorithm
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][k] != Integer.MAX_VALUE && matrix[k][j] != Integer.MAX_VALUE) {
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == Integer.MAX_VALUE) {
+                    matrix[i][j] = -1;
+                }
+            }
+        }
+    }
+}
